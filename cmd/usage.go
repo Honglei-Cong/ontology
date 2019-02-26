78e1a2ede3efb8c0502d0e4598b968e0f4bd68e4
qiluge <wangcheng@onchain.com>
2019-02-26 15:23:39 +0800

    add shard rpc and rest port

--- cmd/usage.go
+++ cmd/usage.go
@@ -263,6 +263,8 @@ var AppHelpFlagGroups = []flagGroup{
 			utils.ParentShardIDFlag,
 			utils.ParentShardIPFlag,
 			utils.ParentShardPortFlag,
+			utils.ShardRestEnableFlag,
+			utils.ShardRpcEnableFlag,
 		},
 	},
 }
